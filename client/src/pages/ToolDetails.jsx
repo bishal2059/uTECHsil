08c23297da9dca0ca388b1c6b85bb6eb64127f48
Binaya17 <binaya.sap12@gmail.com>
2024-06-17 00:10:40 +0545

    edited styles

--- client/src/pages/ToolDetails.jsx
+++ client/src/pages/ToolDetails.jsx
@@ -68,6 +68,9 @@ const ToolDetails = () => {
                         )}
                     </div>
                 </div>
+                <div>
+                    <button className={classes.buy}> Buy Now</button>
+                </div>
             </div>
         </div>
     )
