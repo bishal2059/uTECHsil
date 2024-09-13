2dbe8ce2957368681b882bb65196ec030b393c29
bishal2059 <bishal.sap21@gmail.com>
2024-09-13 23:55:24 +0545

    bug fixes

--- client/src/pages/Tools.jsx
+++ client/src/pages/Tools.jsx
@@ -8,22 +8,22 @@ const Tools = () => {
             <h1 className="text-3xl text-center font-bold">Traditional Tools</h1>
 
             <div className="p-4 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
-                {Object.keys(toolsList).map(culture => {
+                {Object.keys(toolsList).map(tools => {
                     return (
-                        <div key={culture} className={classes.main}>
-                                <Link to={`/tools/${culture}`} className="w-full">
+                        <div key={tools} className={classes.main}>
+                                <Link to={`/tools/${tools}`} className="w-full">
                             <div className="rounded overflow-hidden shadow-lg">
                                 <img
                                     className={classes.pic}
-                                    src={toolsList[culture]['image']}
-                                    alt={toolsList[culture]['name']}
+                                    src={toolsList[tools]['image']}
+                                    alt={toolsList[tools]['name']}
                                 />
                                 <div className="px-6 py-4">
                                     <div className="font-bold text-xl mb-2 ">
-                                        {toolsList[culture]['name']}
+                                        {toolsList[tools]['name']}
                                     </div>
                                     <p className="text-gray-700 text-base text-ellipsis h-20 overflow-hidden">
-                                       <div className={classes.discp}> {toolsList[culture]['description']}
+                                       <div className={classes.discp}> {toolsList[tools]['description']}
                                     </div>
                                     </p>
                                 </div>
